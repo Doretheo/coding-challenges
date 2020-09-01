f58a00f7aadc527b9228815ae44cd23fceab674a
Theodore Nguyen <theodorenguyen5@gmail.com>
2020-08-31 20:19:08 -0700

    Attempted the problem

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -31,8 +31,52 @@
 */
 
 // Feel free to add helper functions if needed.
+// step 1: starting at the first element of an array and comparing it to the second element
+// step 2: if the first element is greater than the second element, it swaps the two
+// step 3: It then compares the second to the third, and the third to the fourth, and so on
+// step 4: the largest values "bubble" to the end of the array. Once it gets to the end of the array, it starts over and repeats the process until the array is sorted numerically.
 
+// input: takes an array
+// output: sorted array
+// edge cases: if the array is length of 0 or 1, return the array
+// constraints: array greater than length of 0
+
+// ANSWER: time complexity =
 
 var bubbleSort = function(array) {
-  // Your code here.
+  // since they don't ask to modify exisiting or create new array, I will create a new one
+  var sorted = [];
+  // check the constraints
+  if (array.length === 0) {
+    return [];
+  }
+  if (array.length === 1) {
+    return array;
+  }
+  // set a variable to the first element/index MAYBE
+//   let currentement = sorted[0];
+//   let lastElement = sorted[sorted.length - 1];
+  // use splice
+  // iterate over the array
+  sorted.push(array[0])
+  for (var i = 1; i < array.length; i++) {
+    let current = array[i];
+    let last = array[array.length - 1];
+    console.log(current, sorted, 'before');
+
+  // if the second indexed element is > the first
+  // if swapped, make sure to check the NEW second number to third
+
+    if (current < sorted[0]) {
+        sorted.unshift(current)
+    } else if (current >= sorted[i]) {
+        sorted.splice(i, 0, current);
+    } else if (current >= sorted[sorted.length - 1]) {
+        sorted.push(current);
+    }
+    console.log(current, sorted, 'after');
+  }
+  return sorted;
 };
+
+bubbleSort([1,4,5,6,7,2]);
