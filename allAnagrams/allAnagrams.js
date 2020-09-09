f00fd9a521225a79f91966519f1b8a87d1705ec6
Theodore Nguyen <theodorenguyen5@gmail.com>
2020-09-08 19:58:33 -0700

    Couldn't complete, will refractor

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,6 +12,38 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
-  // Your code here.
+ var allAnagrams = function(string) {
+  // check for edge cases
+  if (typeof string === 'string') {
+    string = string.split('');
+  } else {
+    string = string.toString().split('');
+  }
+
+  // add a container to hold all the anagrams
+  let anagrams = [];
+  anagrams.push(string.join(''))
+  // console.log(anagrams)
+
+  // create a for loop to iterate through the string array
+  for (var i = 0; i < 100; i++) {
+     anagrams.push(mixingString(i, i+1, string));
+  }
+
+  // return anagrams
+  return anagrams.filter(unique);
 };
+
+var unique = function(value, index, array) {
+  return array.indexOf(value) === index;
+}
+
+var mixingString = function (current, next, array) {
+  let temp = array[current];
+  array[current] = array[next];
+  array[next] = temp;
+  return array.join('');
+}
+
+// debugger;
+allAnagrams('abc');
