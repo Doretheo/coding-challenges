99bf48e9b3f2369549dfe3bf4c4070ad28e20dc5
Theodore Nguyen <theodorenguyen5@gmail.com>
2020-09-25 19:47:53 -0700

    Complete

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -13,5 +13,27 @@
 
 
 var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+  const common = [];
+  let strSplitOne = string1.split('');
+  let strSplitTwo = string2.split('');
+
+  if (string1.length === 0) {
+    return string2;
+  } else if (string2.length === 0) {
+    return string1;
+  } else if (string1 === string2) {
+    return string1;
+  }
+
+  for (var i = 0; i < string1.length; i++) {
+    for (var j = 0; j < string2.length; j++) {
+      if (strSplitOne[i] === strSplitTwo[j] && common.indexOf(string2[j]) < 0) {
+        common.push(string2[j]);
+      }
+    }
+  }
+  return common.join('');
 };
+
+// console.log(commonCharacters('awgeagfers', 'awgrgsoad'));
+// console.log(commonCharacters('1581932847', '32418927512'));
