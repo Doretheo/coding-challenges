5dc2d4c064f5c6d5c05fa3c1afc79f92e3735c76
Theodore Nguyen <theodorenguyen5@gmail.com>
2020-10-16 19:39:48 -0700

    attempted

--- fractionConverter/fractionConverter.js
+++ fractionConverter/fractionConverter.js
@@ -1,17 +1,27 @@
 /**
- * Write a function that takes a number as its argument and 
+ * Write a function that takes a number as its argument and
  * returns a string that represents that number's simplified fraction.
  *
  * Example: toFraction(0.5) === '1/2'
- * 
+ *
  * Whole numbers and mixed fractions should be returned as irregular fractions
- * 
+ *
  * Example: toFraction(3.0) === '3/1'
- * 
+ *
  * Example: toFraction(2.5) === '5/2'
  *
  */
 
 var toFraction = function(number) {
   // Your code here
+  let wholeNum = number.toString().split('.');
+  if (Number(wholeNum[0]) === parseInt(number)) {
+    return `${parseInt(number)}/1`;
+  } else {
+    let numerator = Number(wholeNum[0]);
+    let denominator = Number(`0.${wholeNum[1]}`);
+    numerator += numerator + (Math.floor(denominator) * number);
+    denominator += denominator + (Math.ceil(denominator) * number);
+    return `${numerator}/${denominator}`
+  }
 };
