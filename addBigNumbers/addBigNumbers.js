ddf1102f97a89179c580ae858c07984fdceb6002
Theodore Nguyen <theodorenguyen5@gmail.com>
2021-01-12 00:14:04 -0800

    Daily coding problem

---
+++ addBigNumbers/addBigNumbers.js
@@ -0,0 +1,7 @@
+// Create a function that takes in 2 LARGE numbers as as strings and adds them
+function addBigNumbers(a, b) {
+  return Number(a) + Number(b);
+}
+
+addBigNumbers("10372", "69") // "10441"
+addBigNumbers('63829983432984289347293874', '90938498237058927340892374089') //"91002328220491911630239667963"
