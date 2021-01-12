9cc715eec3754ac209e3e97e6e73bbb25bfbb591
Theodore Nguyen <theodorenguyen5@gmail.com>
2021-01-12 00:03:57 -0800

    Daily algorithm practice

--- sumFibs.js/sumFibs.js
+++ sumFibs.js/sumFibs.js
@@ -1,7 +1,17 @@
 // Create a function that takes an argument n and sums even Fibonacci numbers up to n's index in the Fibonacci sequence.
 function sumFibs(number) {
+  let [a, b] = [0, 1];
+  let sum = 0;
 
+  while (number > 0) {
+    [a, b] = [b, a + b];
+    if (a % 2 === 0) {
+      sum += a;
+    }
+    number -= 1;
+  }
+  return sum
 }
 
-sumFibs(5) === 2 // (0, 1, 1, 2, 3, 5);
+sumFibs(7) === 2 // (0, 1, 1, 2, 3, 5);
 sumFibs(9) === 44; // (0, 1, 1, 2, 3, 5, 8, 13, 21, 34)
