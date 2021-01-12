25234985333bde89b147d8aad6851c44f11dcc21
Theodore Nguyen <theodorenguyen5@gmail.com>
2021-01-11 23:49:40 -0800

    Daily alogirthm problem

---
+++ sumFibs.js/sumFibs.js
@@ -0,0 +1,7 @@
+// Create a function that takes an argument n and sums even Fibonacci numbers up to n's index in the Fibonacci sequence.
+function sumFibs(number) {
+
+}
+
+sumFibs(5) === 2 // (0, 1, 1, 2, 3, 5);
+sumFibs(9) === 44; // (0, 1, 1, 2, 3, 5, 8, 13, 21, 34)
